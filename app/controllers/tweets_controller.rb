4a1908aed01bfc13e3e90e75fded6da297312218
Mario Andrés Correa <a@macool.me>
2014-07-17 15:42:20 -0500

    remove type of results

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
   end
 
   def refresh
-    tweets = Twitter.search APP_CONFIG["twitter"]["query"], result_type: "recent", since_id: Tweet.since_id
+    tweets = Twitter.search APP_CONFIG["twitter"]["query"], since_id: Tweet.since_id
     tweets.results.each do |tweet|
       Tweet.find_or_initialize_by_id_str(tweet.attrs[:id_str]).tap do |t|
         t.update_attributes! tweet.attrs
